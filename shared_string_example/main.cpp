0fb922256eab1e4645b6be0b43384d97ed4e3d14
oleg-m1973 <oleg-m1973@yandex.ru>
2019-02-13 16:46:18 +0300

    Add files via upload

--- shared_string_example/main.cpp
+++ shared_string_example/main.cpp
@@ -1,3 +1,6 @@
+// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "../shared_string.h"
 #include "../cow_string.h"
 
@@ -13,7 +16,7 @@ int main()
 	std::cout << "shared_string(std::string_view): " << shared_string(src2) << std::endl;
 	std::cout << "shared_string(size_t, char): " << shared_string(10, '!') << std::endl;
 	std::cout << "shared_string(char): " << shared_string('!') << std::endl;
-
+	
 	std::cout << "shared_string(size_t, size_t(char *, size_t)): " << shared_string(10, [](char *dst, size_t sz)
 	{
 		for (size_t i = 0; i < sz; ++i, ++dst)
@@ -22,7 +25,16 @@ int main()
 		return sz;
 	}) << std::endl;
 
-	shared_string s = make_shared_string(src1, ' ', src2, repeat_char(2, '!'), '!');
+	std::cout << "shared_string(void(shared_string_creator &)): " << shared_string([&](auto &s)
+	{
+		s.reserve(src1.size() + 1 + src2.size() + 3);
+		s += src1;
+		s += ' ';
+		s += src2;
+		s.append(3, '!');
+	}) << std::endl;
+
+	shared_string s = make_shared_string(src1, ' ', src2, repeat_char(3, '!'));
 	std::cout << "make_shared_string: " << s << std::endl;
 
 	cow_string cow = s;
